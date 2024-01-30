a1e87573a2a5c4b398d0694fecaaf329040c4cb4
abis001 <abis0014@student.monash.edu>
2024-01-30 21:33:19 +1100

    multiskill

--- src/skills/multiskill_utils.py
+++ src/skills/multiskill_utils.py
@@ -14,13 +14,13 @@ from collections import defaultdict, Counter
 #     load_s3_json,
 # )
 
-from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import (
-    confusion_matrix,
-    ConfusionMatrixDisplay,
-    classification_report,
-)
+# from sklearn import svm
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import (
+#     confusion_matrix,
+#     ConfusionMatrixDisplay,
+#     classification_report,
+# )
 
 import spacy
 from spacy import displacy
