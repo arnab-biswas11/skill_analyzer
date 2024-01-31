99f6282b12ac8703d0cbde42c6853681a03e4bb1
abis001 <abis0014@student.monash.edu>
2024-01-31 16:36:40 +1100

    cv upload

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import PyPDF2 as pdf
+
 from src.data_extractor.web_scapper import web_scrap
 from src.skill_extract import pull_skill
 
 scrapped_data = None
 extracted_skills = None
 
-def web_scrap_jobs(sp_data,text_page_num, input_page_limit):
+def web_scrap_jobs(sp_data,text_page_num, input_page_limit,input_role):
     jobs = []
     jd_list = []
     prog = 0
@@ -44,19 +46,26 @@ def web_scrap_jobs(sp_data,text_page_num, input_page_limit):
             text_page_num.text("")
             progress_bar_container.empty()
             n_jobs = len(jobs)
-            st.success(f"Scrapped {n_jobs} job listings successfully!")
+            if n_jobs>0:
+                st.success(f"Scrapped {n_jobs} job listings successfully!")
+            else:
+                st.error(f"No ads with job title {input_role}, please re-enter a new role (example role: Data Analyst)")
 
     return jobs, jd_list
 
 def extract_skills_from_scapped(jd_list):
             
-    data = pull_skill(jd_list).skill_ext()
+    skill_list = pull_skill(jd_list).skill_ext()[1]
+    skill_freq = pull_skill(jd_list).skill_ext()[0]
 
-    top_skills = dict(data.most_common(10))
+    top_skills = dict(skill_freq.most_common(10))
     skills_df = pd.DataFrame(list(top_skills.items()), 
                                 columns=['Skill', 'Number of Job Ads'])
 
-    # Create Altair chart
+    return skill_list, skill_freq, skills_df
+
+def plot(skills_df):
+        # Create Altair chart
     chart = alt.Chart(skills_df).mark_bar().encode(
         x=alt.X('Skill:O', 
                 sort=alt.EncodingSortField(field='Number of Job Ads', 
@@ -67,25 +76,43 @@ def extract_skills_from_scapped(jd_list):
     )
 
     st.altair_chart(chart, use_container_width=True)
-            
 
+def input_pdf_text(uploaded_file):
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
+    return text
 
 def main():
 
-    st.title("Skill Analyzer")
+    st.sidebar.title("zoomJD - Automate the hunt for a right fit job")
+    
+    with st.sidebar:
 
-    st.write("Welcome to the Skill Analyzer app! \
-             This tool helps you scrap data from \
-             current job listings at www.seek.com.au \
-              and visualise the most in demand skills for \
-              the role of your choice.")
 
-    st.write("To get started, enter the desired role and select \
-              the page count, then click 'Click to evaluate'. \
-             Selecting a higher page count would take more time to \
-             extract live job postings but would also extract more jobs!")
+        st.write("Welcome to the zoomJD app! \
+                This tool helps you scrap data from \
+                current job listings at www.seek.com.au \
+                and visualise the most in demand skills for \
+                the role of your choice.")
+    
+        st.markdown("---")
+    
+        st.write("To get started, enter the desired role, select \
+                the page count and upload your resume (optional) in pdf format, then click 'Click to evaluate'. \
+                Selecting a higher page count would take more time to \
+                extract live job postings but would also extract more jobs!")
 
-    st.markdown("---")
+        st.markdown("---")
+        
+        st.write("The app is open source and is in WIP mode with more features being tested. Connect with me to discuss or provide feedback! https://www.linkedin.com/in/arnab-biswas-b6b064132/")
+
+
+        st.write(" ")
+
+    st.title("zoomJD")
 
     # Initialize session_state variables
     if 'confirmation_button' not in st.session_state:
@@ -94,9 +121,11 @@ def main():
         st.session_state.input_page_limit = ""
 
     # User inputs
-    input_role = st.text_input("Enter role:")
-    input_page_limit = st.selectbox('Select page count', ('1', '2', '5', '10', '20', 'all pages'))
+    input_role = st.text_input("Enter role")
+    input_page_limit = st.selectbox('Number of pages to be scrapped', ('1', '2', '5', '10', '20', 'all pages'))
     
+    uploaded_file=st.file_uploader("Upload your resume in PDF format",type="pdf",help="Please uplaod the pdf")
+
     # Confirmation button
     confirmation_button = st.button("Click to evaluate")
 
@@ -118,27 +147,60 @@ def main():
             if scrapped_data is None:
                 scrapped_data = web_scrap(job_title=input_role).extract_jobs()
                 text_page_num = st.text("")
-                return_jobs = web_scrap_jobs(scrapped_data,text_page_num,input_page_limit)
+                return_jobs = web_scrap_jobs(scrapped_data,text_page_num,input_page_limit,input_role)
                 jobs = return_jobs[0]
                 jd_list = return_jobs[1]
-
-            jobs_df = pd.DataFrame(jobs)
-            header_df = pd.json_normalize(jobs_df['header'])
-            jobs_df = pd.concat([jobs_df['url'], header_df], axis=1)
-
-            st.write("Download web scapped jobs as CSV")
-            st.download_button(
-                label=':anchor:',
-                data=jobs_df.to_csv(index=False, encoding='utf-8', errors='replace'),
-                file_name='jobs_data.csv',
-                mime='text/csv')
             
             st.markdown("---")
 
-            with st.spinner("Visualising top skills from scanned job descriptions..."):
-                extract_skills_from_scapped(jd_list)
+            if len(jd_list)>0:
+                jobs_df = pd.DataFrame(jobs)
+                header_df = pd.json_normalize(jobs_df['header'])
+                jobs_df = pd.concat([jobs_df['url'], header_df], axis=1)
+
+                with st.spinner("Visualising top skills from scanned job descriptions..."):
+
+                    try:
+                        extracted_skills = extract_skills_from_scapped(jd_list)
+                        plot(extracted_skills[2])
+                        jobs_df['extracted_skills'] = extracted_skills[0]
+
+                    except Exception as e:
+                        print(e)
+                        st.error('No skills to extract, please retry with a different role')
+
+                if uploaded_file is not None:
+
+                    try:
+                        cv_details=input_pdf_text(uploaded_file)
+                        personal_skills = extract_skills_from_scapped([cv_details])[0][0]
+                        jobs_df['matched_skills'] = jobs_df['extracted_skills'].apply(lambda x: list(set(x) & set(personal_skills)))
+                        jobs_df['unmatched_skills'] = jobs_df['extracted_skills'].apply(lambda x: list(set(x) - set(personal_skills)))
+
+                    except Exception as e:
+                        print(e)
+                        st.error('No skills to extract, please retry with a different role')
+
+                
+                st.download_button(
+                    label='Download job details :anchor:',
+                    data=jobs_df.to_csv(index=False, encoding='utf-8', errors='replace'),
+                    file_name='jobs_details.csv',
+                    mime='text/csv')
+                
+                try:
+                    skill_freq = pd.DataFrame(list(extracted_skills[1].items()), columns=['Skill', 'Value'])
+                    st.download_button(
+                        label='Download skill frequency :anchor:',
+                        data=skill_freq.to_csv(index=False, encoding='utf-8', errors='replace'),
+                        file_name='skill_frequency.csv',
+                        mime='text/csv')                    
+
+                except Exception as e:
+                    print(e)
 
 
+            
 # Run the app
 if __name__ == "__main__":
     main()
--- src/skill_extract.py
+++ src/skill_extract.py
@@ -17,15 +17,23 @@ class pull_skill:
         job_skills_matched = es.extract_skills(self.job_details)
 
         skill_counter = Counter()
+        skills_list = []
 
         for entry in job_skills_matched:
             skills = entry['SKILL']
+            skills_in_job = []
+            
             for potential_skill, (hier_skill, identifier) in skills:
                 # Determine whether to use the skill or the identifier
-                # skill_to_count = hier_skill if len(identifier) != 20 else potential_skill
-                skill_counter[potential_skill] += 1
+                skill_to_count = hier_skill if len(identifier) != 20 else potential_skill
+                skill_counter[skill_to_count] += 1
+                skills_in_job.append(skill_to_count)
 
-        return skill_counter
+            skills_list.append(skills_in_job)
+
+        return skill_counter, skills_list
+
+             
     
 # x = pull_skill(["Your career in healthcare made personal At Medibank we've recently unified our health services under one brand - Amplar Health.  With over 1000 employees, our nurses, GPs and other amazing allied health professionals our focus is on improving healthcare experiences and championing greater access, choice, and control for people in Australia when it comes to managing their health.   The Opportunity Are you someone passionate about creating and maintaining best in-class preventative health programs? If so, we have a new and exciting opportunity to join our team at Amplar health!!! To better support the prevention ambition for Medibank, Amplar Health's Prevention Hub has recently been established.  As a key member of the Amplar Health Prevention Squad, the Product Owner, will work closely with the Prevention Squad/Hub, Health Operations, Business Development, Marketing, Customer Experience, Tech & Ops including Digital, amongst other stakeholders. As a Product owner you will act as subject matter expert in a portfolio of preventative health programs. You will drive the design, development, re-design, and improvement of the preventative health programs, develop and execute a portfolio strategy, including growth strategy that can lead to maximising our Prevention Health programs to make them commercially successful. For the right person, the role could be based anywhere with flexibility to work in the office or from home as needed. Come and join a dynamic and talented team that is looking to change the way of delivering health care in Australia! About You  key to this role someone with proven experience in managing, designing, and delivering health programs, including digital/clinician-based programs. You will have exceptional product expertise and extensive networks and influence in the broader health and digital ecosystem. You will have significant experience in both commercial and start up environments. Excellent project management and prioritisation skills with the ability to juggle multiple competing priorities.   Our much-loved PerksAlong with joining an outstanding organisation that is transforming the delivery of healthcare, working at Amplar health also means:    Great work life balance Impressive, 35% subsidised Medibank/ahm health insurance (which can also include your pets, cars, upcoming travel, and house!)!  Right now, our employees also have a 25% discount on travel insurance.  14-weeks paid parental leave for all eligible employees, regardless of whether you are the primary or secondary carer - in fact, we're the proud winners of the number one workplace for dads.  Feel Good Health Hub - a digital platform for your mobile that gives you access to a range of clinical health and wellbeing services at your fingertips - the ease in booking appointments with trusted professionals such as a health & wellbeing check in.  Great Discounts - as an employee you can access various discounts across a range of partners including Apple, Samsung, Telstra, HP, SpecSavers, Cinema & Theme park tickets (Spendless), Wyndham Travel and more.   Learn more about Amplar Health at https://amplarhealth.com.au We celebrate diversity of thought because we want to make better decisions for our customers and our people. As we work towards our goal of making health personal, we value the knowledge and contribution of Aboriginal and Torres Strait Islanders.  We're also committed to supporting better accessibility for our customers and our people. We encourage you to talk to us about any adjustment or additional support you may require.  Employees in face-to-face healthcare roles will be required to be fully vaccinated for COVID-19 as a condition of employment. "]).skill_ext()
 # print(x)
